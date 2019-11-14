a7ae7f7082990edeb104eb3ff4a0e21f781f1f59
Tim Kye <Timothy.Kye@nike.com>
2019-11-14 13:59:50 -0800

    ugrade to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,27 +1,28 @@
 resource "aws_api_gateway_method" "ResourceOptions" {
-  rest_api_id   = "${var.rest_api_id}"
-  resource_id   = "${var.resource_id}"
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "ResourceOptionsIntegration" {
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.ResourceOptions.http_method
   type        = "MOCK"
 
   request_templates = {
     "application/json" = <<PARAMS
 { "statusCode": 200 }
 PARAMS
+
   }
 }
 
 resource "aws_api_gateway_method_response" "ResourceOptions200" {
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.ResourceOptions.http_method
   status_code = "200"
 
   response_models = {
@@ -36,10 +37,10 @@ resource "aws_api_gateway_method_response" "ResourceOptions200" {
 }
 
 resource "aws_api_gateway_integration_response" "ResourceOptionsIntegrationResponse" {
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
-  status_code = "${aws_api_gateway_method_response.ResourceOptions200.status_code}"
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.ResourceOptions.http_method
+  status_code = aws_api_gateway_method_response.ResourceOptions200.status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
@@ -47,3 +48,4 @@ resource "aws_api_gateway_integration_response" "ResourceOptionsIntegrationRespo
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
+
