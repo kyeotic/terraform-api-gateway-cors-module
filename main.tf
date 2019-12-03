723cbea5161ea3036d61893f02c947b2c583d169
Tim Kye <Timothy.Kye@nike.com>
2019-12-03 08:46:12 -0800

    add cache-control

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_api_gateway_integration_response" "ResourceOptionsIntegrationRespo
   status_code = aws_api_gateway_method_response.ResourceOptions200.status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,cache-control'"
     "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
